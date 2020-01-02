d6163574b3113948d89570bce7e5f2bc50782b42
Janice Huang <janicehuang@gmail.com>
2020-01-01 20:53:48 -0800

    add more blacklist terms and add python equivalent of 'uniq'

--- cleanText.py
+++ cleanText.py
@@ -3,7 +3,7 @@ from textblob import TextBlob
 
 # source: https://www.analyticsvidhya.com/blog/2017/01/ultimate-guide-to-understand-implement-natural-language-processing-codes-in-python/
 
-noise_list = ["additional", "and", "bag", "bags", "blanched", "boiled", "chopped", "cold", "cubed", "cup", "cups", "dash", "dashes", "diced", "diced", "drained", "dry", "etc.", "finely", "firmly packed", "for", "fresh", "freshly grated", "frozen", "garnish", "grams", "grated", "ground", "halved", "hard-boiled", "large", "leaves", "liquid", "loaf", "minced", "mixed", "more if needed", "needed", "or", "ounce", "ounces", "oz", "package", "peeled", "pieces", "pitted", "pound", "pounds", "quart", "quarts", "rinsed", "salt", "several", "size", "sliced", "slices", "small", "splashing", "sprigs", "stick", "tablespoon", "tablespoons", "taste", "teaspoon", "teaspoons", "the", "thinly", "to taste", "torn", "used", "very", "warm", "warmed", "weight", "whole", "your"]
+noise_list = ["additional", "and", "bag", "bags", "blanched", "boiled", "chopped", "cold", "cubed", "cup", "cups", "dash", "dashes", "diced", "diced", "drained", "dry", "etc.", "extra", "finely", "firmly packed", "for", "fresh", "freshly grated", "frozen", "garnish", "grams", "grated", "ground", "halved", "hard-boiled", "heaping", "large", "leaves", "liquid", "loaf", "melted", "minced", "mixed", "more if needed", "needed", "or", "ounce", "ounces", "oz", "package", "pancake", "peeled", "pieces", "pitted", "pound", "pounds", "quart", "quarts", "rinsed", "salt", "several", "size", "sliced", "slices", "small", "splashing", "sprigs", "stick", "tablespoon", "tablespoons", "taste", "teaspoon", "teaspoons", "the", "thinly", "to taste", "torn", "used", "very", "warm", "warmed", "weight", "whole", "your", "zest"]
 
 def _remove_noise(input_text):
     words = input_text.split()
@@ -11,11 +11,11 @@ def _remove_noise(input_text):
     noise_free_text = " ".join(noise_free_words)
     return noise_free_text
 
-blob = TextBlob("2 Tablespoons Olive Oil\n2 Tablespoons Butter\n1 pound Scallops\n1 pound Shrimp\n5 cloves Garlic\n3/4 cups Dry White Wine\n28 ounces, weight Whole Or Diced Tomatoes\n Salt And Pepper, to taste\n1/4 teaspoon Crushed Red Pepper\n1/4 cup Heavy Cream, Warmed\n12 whole Basil Leaves Torn\n Chicken Broth, If Needed For Splashing In A Little Liquid\n12 ounces, weight Pasta (I Used Fusilli Bucati, But Any Kind Will Do!)")
+blob = TextBlob("1-1/2 cup Cake Flour\n1 Tablespoon (additional) Cake Flour\n1/4 teaspoon Salt\n1 Tablespoon (heaping) Baking Powder\n3 Tablespoons Sugar\n1-1/2 cup Evaporated Milk (more If Needed)\n1 whole Lemon (more If Needed)\n1 whole Large Egg\n1-1/2 teaspoon Vanilla\n2 Tablespoons Butter, Melted\n Zest From 1 Lemon\n1 cup Heaping Blueberries\n Extra Butter\n Maple Or Pancake Syrup")
 
 wordList = []
 for np in blob.noun_phrases:
   noise_removed_np = _remove_noise(np)
   if noise_removed_np != '':
     wordList.append(noise_removed_np)
-print(wordList)
+print(list(set(wordList)))
