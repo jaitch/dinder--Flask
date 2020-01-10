55e4e5d85421853a9f7239550e32da951a64dd21
Janice Huang <janicehuang@gmail.com>
2020-01-09 23:42:37 -0800

    tweak numbers in SQL query

--- src/connectPandas.py
+++ src/connectPandas.py
@@ -3,18 +3,18 @@ from scipy.spatial.distance import squareform, pdist
 import numpy as np
 import psycopg2
 from sqlalchemy import create_engine
-import itertools
+# import itertools
 
 engine = create_engine('postgres+psycopg2://janicehuang@localhost/dinder')
 
 # Method to compute Jaccard similarity index between two sets
-def compute_jaccard(ing1_recipes, ing2_recipes):
-    intersection = ing1_recipes.intersection(ing2_recipes)
-    union = ing1_recipes.union(ing2_recipes)
-    jaccard = len(intersection)/float(len(union))
-    return jaccard
+# def compute_jaccard(ing1_recipes, ing2_recipes):
+#     intersection = ing1_recipes.intersection(ing2_recipes)
+#     union = ing1_recipes.union(ing2_recipes)
+#     jaccard = len(intersection)/float(len(union))
+#     return jaccard
 
-sql = 'select i.ingredient_id, ri.recipe_id from (select ri.ingredient_id from ingredients i, recipe_ingredients ri where i.id=ri.ingredient_id group by ri.ingredient_id having count(ri.ingredient_id)>5000) as i, recipe_ingredients as ri where i.ingredient_id=ri.ingredient_id limit 100'
+sql = 'select i.ingredient_id, ri.recipe_id from (select ri.ingredient_id from ingredients i, recipe_ingredients ri where i.id=ri.ingredient_id group by ri.ingredient_id having count(ri.ingredient_id)>1000) as i, recipe_ingredients as ri where i.ingredient_id=ri.ingredient_id limit 40000'
 
 df = pd.read_sql_query(sql, con=engine, coerce_float=False, params=None, parse_dates=None)
 
