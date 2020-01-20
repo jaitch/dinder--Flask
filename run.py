cd3c18d6a678c7071193507a94f73f1ca2ae21a0
Janice Huang <janicehuang@gmail.com>
2020-01-19 21:17:55 -0800

    change run file so works with heroku

--- run.py
+++ run.py
@@ -2,8 +2,7 @@ import os
 
 from src.app import create_app
 
-if __name__ == '__main__':
-  env_name = os.getenv('FLASK_ENV')
-  app = create_app(env_name)
+env_name = os.getenv('FLASK_ENV')
+app = create_app(env_name)
 
-  app.run()
+app.run()
