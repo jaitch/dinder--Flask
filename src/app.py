e713032678fcdc9108389e9b14e876b82f3df656
Janice Huang <janicehuang@gmail.com>
2020-02-16 21:58:59 -0800

    commented out mysterious localhost8000 code

--- src/app.py
+++ src/app.py
@@ -73,7 +73,7 @@ def create_app(env_name):
   def get_json(path):
     return send_from_directory('data', path)
 
-    if __name__ == "__main__":
-      app.run(host='0.0.0.0', port=8000, debug=True)
+    # if __name__ == "__main__":
+    #   app.run(host='0.0.0.0', port=8000, debug=True)
 
   return app
